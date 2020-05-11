b0ce9d09e3108588daf50122e3eb8577ba0dbb8d
WADEOS <wade4615@gmail.com>
2020-05-10 19:28:37 -0500

    removed pedump

--- JarvisOSFromScratch/hosttools/tools/pedump/backup/exedump.h
+++
@@ -1,6 +0,0 @@
-//==================================
-// PEDUMP - Matt Pietrek 1997
-// FILE: EXEDUMP.H
-//==================================
-
-void DumpExeFile( PIMAGE_DOS_HEADER dosHeader );
--- JarvisOSFromScratch/hosttools/tools/pedump/backup/resdump.h
+++
@@ -1,1 +0,0 @@
-void DumpResourceSection(DWORD base, PIMAGE_NT_HEADERS pNTHeader);
